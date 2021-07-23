2c03281bf2122655b204623d54a24593a6f78274
Hyzrn <selahattin_hayzaran@hotmail.com>
2021-07-23 19:05:10 +0300

    init

---
+++ src/services/restaurants/restaurants.context.js
@@ -0,0 +1,40 @@
+import React, { useState, createContext, useEffect, useMemo } from "react";
+
+import {
+  restaurantsTransform,
+  restaurantsRequest,
+} from "./restaurants.service";
+
+export const RestaurantContext = createContext();
+
+export const RestaurantsContextProvider = ({ children }) => {
+  const [restaurants, setRestaurants] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const retrieveRestaurants = () => {
+    setIsLoading(true);
+    setTimeout(() => {
+      restaurantsRequest()
+        .then(restaurantsTransform)
+        .then((result) => {
+          setIsLoading(false);
+          setRestaurants(result);
+        })
+        .catch((err) => {
+          setIsLoading(false);
+          setError(err);
+        });
+    }, 2000);
+  };
+
+  useEffect(() => {
+    retrieveRestaurants();
+  }, []);
+
+  return (
+    <RestaurantContext.Provider value={{ restaurants, isLoading, error }}>
+      {children}
+    </RestaurantContext.Provider>
+  );
+};
